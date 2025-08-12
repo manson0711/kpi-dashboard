fcb76051998d2e4ab4a29ed20ded76a15fbb0d3a
manson0711 <info@that-clicks.de>
2025-08-12 09:05:20 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E50846",
+        muted: "#eaeaea",
+        "muted-foreground": "#161A40",
+      },
+    },
   },
+  plugins: [],
 }
