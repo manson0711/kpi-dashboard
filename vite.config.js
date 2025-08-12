4f73d0954b0d5afa2909f1daa207f6463ed4c694
manson0711 <info@that-clicks.de>
2025-08-12 09:14:15 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/kpi-dashboard/',
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
+  css: {
+    postcss: './postcss.config.js',
   },
 })
